4297abd2fb92ee9de2f56f73b119f0c5d347eed7
Agam <agam.brahma@gmail.com>
2021-03-01 22:05:57 +0000

    leveled up with arrays and slices a bit

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,14 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let mut myvec : Vec<u8> = Vec::new();
+    for i in 0..150 {
+        myvec.push(i)
+    }
+    let boxed_slice: Box<[u8]> = myvec.into_boxed_slice();
+
+    let a: &[u8] = &*boxed_slice;
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
